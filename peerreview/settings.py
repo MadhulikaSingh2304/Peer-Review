7e361f6b66f22b1f7ac2411cbe9c7e62ab3909ac
Madhulika Singh <94697495+MadhulikaSingh2304@users.noreply.github.com>
2023-06-03 02:58:19 +0530

    Update settings.py

--- peerreview/settings.py
+++ peerreview/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3$ns+thp#8lmy-i*-xyd#za6*nnqg)ydd$t4*o7s#5cujic_rt'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
